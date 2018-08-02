bddb832cca9249d980833061c6c5932139f3032d
WL600587\lrf0675 <turner.lakeisha@gmail.com>
2018-08-02 16:58:21 -0500

    Adding redux api

---
+++ src/components/posts_index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PostsIndex extends Component {
+    render() {
+        return (
+            <div><b>Posts Index</b></div>
+        );
+    }
+}
+
+export default PostsIndex;
